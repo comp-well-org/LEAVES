bd28efaf9ff20afce3dfc9e6708dd3a4326e4616
yh0903 <652963574@qq.com>
2022-09-11 22:15:59 -0700

    add sagemaker runner

---
+++ run_sagemaker.py
@@ -0,0 +1,28 @@
+import sagemaker
+import boto3
+from sagemaker.pytorch import PyTorch
+
+# sagemaker_session = sagemaker.LocalSession()
+# sagemaker_session.config = {'local': {'local_code': True}}
+
+sagemaker_session = sagemaker.Session()
+
+role = "arn:aws:iam::456181194192:role/service-role/AmazonSageMakerServiceCatalogProductsExecutionRole"
+
+metric_definitions = [
+    {"Name": "EncoderLoss", "Regex": "EncoderLoss.*:\D*(.*?)$"},
+    {"Name": "ViewLoss", "Regex": "ViewLoss.*:\D*(.*?)$"},
+]
+
+estimator = PyTorch(entry_point='main.py',
+                    source_dir= r'C://Users/hello/Projects/viewmaker_physiological',
+                    base_job_name = 'ViewMaker-sleep-edfe',
+                    role=role,
+                    py_version='py3',
+                    framework_version='1.8.0',
+                    instance_count=1,
+                    instance_type='ml.p3.2xlarge',
+                    metric_definitions=metric_definitions
+                    )
+
+estimator.fit({'training': 's3://compwell-databucket/processed_DA/ptbxl/100hz/'})
